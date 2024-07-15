84605faabab345b2a45c17d6a611ff0816e0cf49
Douglas Mendes <douglasleomendes@hotmail.com>
2024-07-15 20:28:57 +0100

    update go version

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
@@ -13,11 +14,18 @@ func main() {
 	// Get environment variables
 	repoOwner := os.Getenv("GITHUB_REPOSITORY_OWNER")
 	repoName := os.Getenv("GITHUB_REPOSITORY_NAME")
-	prNumber := os.Getenv("GITHUB_PR_NUMBER")
+	prNumberStr := os.Getenv("GITHUB_PR_NUMBER")
 	githubToken := os.Getenv("GITHUB_TOKEN")
 
-	if repoOwner == "" || repoName == "" || prNumber == "" || githubToken == "" {
+	if repoOwner == "" || repoName == "" || prNumberStr == "" || githubToken == "" {
 		fmt.Println("Some environment variables are missing")
+		return
+	}
+
+	// Convert prNumber to int
+	prNumber, err := strconv.Atoi(prNumberStr)
+	if err != nil {
+		fmt.Printf("Error converting PR number to int: %v\n", err)
 		return
 	}
 
@@ -33,7 +41,7 @@ func main() {
 	comment := &github.IssueComment{
 		Body: github.String("Hello World"),
 	}
-	_, _, err := client.Issues.CreateComment(ctx, repoOwner, repoName, prNumber, comment)
+	_, _, err = client.Issues.CreateComment(ctx, repoOwner, repoName, prNumber, comment)
 	if err != nil {
 		fmt.Printf("Error creating comment: %v\n", err)
 		return
